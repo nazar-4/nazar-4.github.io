bbc29113d5187b8290497d308b0b8fb07afbc12c
nazar-4 <nazar.boichuk-ki2101@kep.nung.edu.ua>
2024-12-12 19:37:47 +0200

    Create script8.js

---
+++ script8.js
@@ -0,0 +1,23 @@
+// script.js
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
